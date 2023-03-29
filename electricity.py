fab72e7c5e4312c0418abb6315308514b287541a
AleemaSaleem <116712791+AleemaSaleem@users.noreply.github.com>
2023-03-29 15:18:56 +0500

    Update electricity.py

--- electricity.py
+++ electricity.py
@@ -33,10 +33,10 @@ if __name__ == "__main__":
     model = ELECTRICITY(args)
     if torch.cuda.device_count() > 1:
       print("Let's use", torch.cuda.device_count(), "GPUs!")
-  # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+      # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
       model = nn.DataParallel(model)
 
-model.to(device)
+      model.to(device)
     
     trainer = Trainer(args,ds_parser,model)
 
