b6d1829fcd1d6540c7c69cc6875b29705523d9b5
AleemaSaleem <116712791+AleemaSaleem@users.noreply.github.com>
2023-04-06 22:52:05 +0500

    Update Trainer.py
    268,340

--- Trainer.py
+++ Trainer.py
@@ -265,7 +265,9 @@ class Trainer:
         if not os.path.exists(self.export_root):
             os.makedirs(self.export_root)
         print('Saving best model...')
-        torch.save(self.model.state_dict(), self.export_root.joinpath('best_acc_model.pth'))
+        torch.save(self.model.module.state_dict(), self.export_root.joinpath('best_acc_model.pth')) 
+        #torch.save(self.model.state_dict(), self.export_root.joinpath('best_acc_model.pth'))
+        
         #os.chdir(r'/kaggle/working')
        # torch.save(best_acc_model.state_dict(), r'/kaggle/working/NILM-Project/results/refit/TV/best_acc_model.pth')
         #from IPython.display import FileLink
@@ -334,7 +336,8 @@ class Trainer:
         if not os.path.exists(self.export_root):
             os.makedirs(self.export_root)
         print('Saving best model...')
-        torch.save(self.model.state_dict(), self.export_root.joinpath('best_acc_model.pth'))
+        torch.save(self.model.module.state_dict(), self.export_root.joinpath('best_acc_model.pth')) 
+       # torch.save(self.model.state_dict(), self.export_root.joinpath('best_acc_model.pth'))
 
     def _load_best_model(self):
         try:
