22ab1504f5a52ac295bca4eb0bea8d49aef21b7c
AleemaSaleem <116712791+AleemaSaleem@users.noreply.github.com>
2023-07-22 08:22:17 +0500

    Update electricityy.py

--- electricityy.py
+++ electricityy.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         args.house_indicies = [ 2, 3, 4,5, 6]
         ds_parser = Redd_Parser(args)
     elif args.dataset_code == 'uk_dale':
-        args.house_indicies = [1, 3, 4, 5]
+        args.house_indicies = [1, 2, 3, 4, 5]
         ds_parser = UK_Dale_Parser(args)
     elif args.dataset_code == 'refit':
         args.house_indicies = [2,3,16]
